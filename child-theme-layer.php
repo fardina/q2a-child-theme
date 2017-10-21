6b6dd72d4e7940034d2104190a1048911c6b8d71
fardina <fardin.amini@yahoo.com>
2017-10-22 01:55:15 +0330

    Update child-theme-layer.php

--- child-theme-layer.php
+++ child-theme-layer.php
@@ -3,10 +3,10 @@
 class qa_html_theme_layer extends qa_html_theme_base
 {
 	function head_css() {
+		parent::head_css();
 		if (qa_opt('child_theme_on')) { // first check the feature is turned on
 		$this->output('<LINK REL="stylesheet" TYPE="text/css" HREF="'.QA_HTML_THEME_LAYER_URLTOROOT.'child-theme.css"/>');
 		}
-        parent::head_css();
 		
        }
 }
